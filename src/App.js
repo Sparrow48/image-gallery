4299901e9f82db4cc494fd6e37b0214f0f0c7c0a
sparrow48 <abunasib48@gmail.com>
2021-11-02 18:22:07 +0600

    use pixabay api for images

--- src/App.js
+++ src/App.js
@@ -1,44 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 function App() {
-  return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg ">
-      <img
-        src="https://source.unsplash.com/random"
-        alt="random"
-        className="w-full"
-      />
-      <div className="px-6 py-4">
-        <div className="font-bold text-purple-500 text-xl md-2">
-          Photo by Hasib
-        </div>
-        <ul>
-          <li>
-            <strong>Views:</strong>
-            400
-          </li>
-          <li>
-            <strong>Downloads:</strong>
-            300
-          </li>
-          <li>
-            <strong>Likes:</strong>
-            4000
-          </li>
-        </ul>
-      </div>
-      <div className="px-6 py-4">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag1
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag2
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag3
-        </span>
-      </div>
-    </div>
-  );
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = true;
+  const [term, setTerm] = "";
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_API_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+    ).then((res) => {
+      setImages(data.hits);
+      setIsLoading(false);
+    });
+  }, []);
+
+  return <div></div>;
 }
 
 export default App;
